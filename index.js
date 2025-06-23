0a0619ccc373f0b11deca7abdd201631398375b1
Hayden Dingman <haydendingman@gmail.com>
2025-06-23 14:51:53 -0700

    Add time to dashboard

--- index.js
+++ index.js
@@ -32,3 +32,11 @@ fetch(`https://api.coingecko.com/api/v3/coins/dogecoin`)
     .catch(err => {
         console.log(err)
     });
+
+function createDate() {
+    const date = new Date();
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "short"})
+}
+
+createDate();
+setInterval(createDate, 1000);
